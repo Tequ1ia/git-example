33845641c26063ef25c44f38b9b46e4c541f02f1
liyuu <502023240047@smail.nju.edu.cn>
2024-02-10 15:09:47 +0800

    edit a.cpp

--- a.cpp
+++ a.cpp
@@ -1,1 +1,10 @@
+/*
+ * @Author: liyuu 502023240047@smail.nju.edu.cn
+ * @Date: 2024-02-08 22:11:20
+ * @LastEditors: liyuu 502023240047@smail.nju.edu.cn
+ * @LastEditTime: 2024-02-10 14:27:17
+ * @FilePath: /git-example/a.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 aaa
+aaaaaaa
